6888653d61d03bc9747390e14da26d763e9a781f
Kyle <kyle_connor1989@hotmail.com>
2022-04-17 02:14:03 +0800

    End of Section 18

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -1,3 +1,4 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { Card, Icon, Image } from 'semantic-ui-react';
@@ -7,17 +8,23 @@ interface Props{
     profile: Profile;
 }
 
-export default function ProfileCard({profile}:Props) {
+export default observer(function ProfileCard({profile}:Props) {
+
+  function truncate(str: string | undefined){
+    if(str)
+      return str.length > 40 ? str.substring(0,17) + '...' : str;
+  }
+
   return (
     <Card as={Link} to={`/profiles/${profile.username}`}>
         <Image src={profile.image || 'assets/user.png'} />
         <Card.Content>
             <Card.Header>{profile.displayName}</Card.Header>
-            <Card.Description>Bio goes here</Card.Description>
+            <Card.Description>{truncate(profile.bio)}</Card.Description>
         </Card.Content>
         <Card.Content extra>
             <Icon name='user' />20 followers
         </Card.Content>
     </Card>
   )
-}
+})
